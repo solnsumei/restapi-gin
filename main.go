c18ae1f7b19943bd3a049f53dee1471a29df4ee4
Solomon Nsumei <solnsumei@gmail.com>
2022-12-09 11:31:01 +0100

    add controller and endpoint to fetch all books

--- main.go
+++ main.go
@@ -4,14 +4,24 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/solnsumei/restapi-gin/controllers"
+	"github.com/solnsumei/restapi-gin/models"
 )
 
 func main() {
 	router := gin.Default()
 
+	// Initialize DB
+	models.ConnectDatabase()
+
 	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"data": "Hello API"})
+		c.JSON(http.StatusOK, gin.H{"data": "Hello Book API"})
 	})
+
+	booksRouter := router.Group("/books")
+	{
+		booksRouter.GET("/", controllers.FindBooks)
+	}
 
 	router.Run("localhost:3000")
 }
